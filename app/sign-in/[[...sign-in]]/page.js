f9302d67996f34fe3b78ddaf67e32286e728021c
Deanna <holtd1031@gmail.com>
2024-08-17 19:47:06 -0700

    I have added the sign in, sign up, and pricing sec tion.

---
+++ app/sign-in/[[...sign-in]]/page.js
@@ -0,0 +1,27 @@
+import { SignIn } from "@clerk/nextjs";
+import { Container, Toolbar, Typography, AppBar, Button, Link, Box } from "@mui/material";
+
+export default function SignUpPage() {
+    return (
+        <Container maxWidth="100vw"> 
+            <AppBar position="static" sx={{backgroundColor: '#3f51b5'}}>
+                <Toolbar>
+                    <Typography variant="h6" sx={{flexGrow: 1,}}>
+                        Flashcard Saas
+                    </Typography>
+                    <Button color='inherit'>
+                        <Link href='/sign-in' passHref sx={{textDecoration: 'none', color: '#FFFFFF'}}>Login</Link>
+                    </Button>
+                    <Button color='inherit'>
+                        <Link href='/sign-up' passHref sx={{textDecoration: 'none', color: '#FFFFFF'}}>Sign Up</Link>
+                    </Button>
+                </Toolbar>
+            </AppBar>
+
+            <Box display='flex' flexDirection='column' alignItems='center' justifyContent='center'>
+                <Typography variant="h4" sx={{p: 2}}>Sign In</Typography>
+                <SignIn />
+            </Box>
+        </Container>
+    )
+}
